087c4055a644b3b3c8e5beced68aac65b3f5e3f9
User <User@DESKTOP-JVPGF5F>
2024-07-29 10:36:08 +0530

    Changes in Footer

--- ADSInfoSoft/obj/Debug/net8.0/ADSInfoSoft.AssemblyInfo.cs
+++ ADSInfoSoft/obj/Debug/net8.0/ADSInfoSoft.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ADSInfoSoft")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67dbd461d75ecd8f53156ecd4e43489d6986addb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ADSInfoSoft")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ADSInfoSoft")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
