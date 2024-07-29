67dbd461d75ecd8f53156ecd4e43489d6986addb
User <User@DESKTOP-JVPGF5F>
2024-07-29 10:25:37 +0530

    Base Branch

---
+++ ADSInfoSoft/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ADSInfoSoft.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
